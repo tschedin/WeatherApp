1993d6b867e701588dedeee8a5fed495d8e44420
tschedin <toms@tempworks.com>
2019-09-27 16:12:36 -0500

    Adding radio buttons

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -4,13 +4,29 @@ class WeatherApp extends Component {
   render() {
     return (
       <div className="weatherAppMain">
+
         <div className="header">
           <form>
             <input placeholder="enter zipcode">
             </input>
             <button type="submit">submit</button>
+          
+            <div className="radio">
+          <label>
+            <input type="radio" value="option1" checked={true} />
+            Actual Temp
+          </label>
+
+          <label>
+            <input type="radio" value="option2" />
+            Real Feel
+          </label>
+            </div>
+          
+          
           </form>
-        </div>
+         
+        </div> 
       </div>
     );
   }
