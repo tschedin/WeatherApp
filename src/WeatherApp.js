b4fe76b8202a9bba29872250bb8d9c224c6cd2d5
tschedin <toms@tempworks.com>
2019-10-01 12:19:30 -0500

    Starting to move away from radio buttons

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,35 +1,54 @@
 import React, { Component } from "react";
- 
+import WeatherAppTempType from "./WeatherAppTempType";
+
 class WeatherApp extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: []
+        };
+
+        this.addZip = this.addZip.bind(this);
+    }
+
+    addZip(e) {
+if (this._inputElement.value !=="") {
+    var newZip = {
+        text: this._inputElement.value
+    };
+
+    this.setState((prevState) => {
+        return {
+            items: prevState.items.concat(newZip)
+        };
+    });
+
+    this._inputElement.value = "";
+}
+
+console.log(this.state.items);
+
+e.preventDefault();
+
+    }
+
   render() {
     return (
       <div className="weatherAppMain">
 
         <div className="header">
-          <form>
-            <input placeholder="enter zipcode">
-            </input>
-            <button type="submit">submit</button>
-          
-            <div className="radio">
-          <label>
-            <input type="radio" value="option1" checked={true} />
-            Actual Temp
-          </label>
-
-          <label>
-            <input type="radio" value="option2" />
-            Real Feel
-          </label>
-            </div>
-          
-          
-          </form>
-         
+                <form onSubmit={this.addZip}>
+                    <input ref={(a) => this._inputElement = a}
+                            placeholder="Zip Code">
+                    </input>
+                    <button type="submit">Submit</button>                
+                </form>
         </div> 
       </div>
-    );
-  }
+    ); 
+  } 
 }
- 
+
 export default WeatherApp;
