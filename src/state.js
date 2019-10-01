5ee1a01af6be447b4fb75bb4b4406e97378dbe40
tschedin <toms@tempworks.com>
2019-10-01 12:19:49 -0500

    related to last push

---
+++ src/state.js
@@ -0,0 +1,18 @@
+
+var React = require('react');
+
+var Application = React.createClass({
+
+  getInitialState: function () {
+    return {
+      selectedOption: 'option1'
+    };
+  },
+
+  render: function () {
+    return ({/* JSX code */});
+  }
+});
+
+module.exports = Application;
+
