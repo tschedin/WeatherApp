6a1d284eb0c4bda9511285d8d0185fa022b7fd69
tschedin <toms@tempworks.com>
2019-10-01 14:26:58 -0500

    creating first components

---
+++ src/Components/titles.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Titles extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Weather Finder</h1>
+                <p>Find out detailed information for weather!                   
+                </p>
+            </div>
+        );
+    }
+};
+
+export default Titles;
---
+++ src/WeatherAppTEST.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Titles from "./Components/titles";
+
+class WeatherApp extends React.Component {
+render() {
+    return (
+    <div>
+        <p>Hello</p>
+        <h1>meow! meow!</h1>
+    </div>
+        );
+}
+
+}
+
+export default WeatherApp;
