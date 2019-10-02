c34c35dcda84f76d77e8960d20a04ff48ca54d32
tschedin <toms@tempworks.com>
2019-10-02 17:02:09 -0500

    adding bootstrap

--- src/WeatherAppTEST.js
+++ src/WeatherAppTEST.js
@@ -47,16 +47,28 @@ class WeatherApp extends React.Component {
 render() {
     return (
     <div>
-          <Titles />      
-          <Form getWeather={this.getWeather}/>      
-          <Weather 
-            temperature={this.state.temperature} 
-            city={this.state.city} 
-            country={this.state.country} 
-            humidity={this.state.humidity} 
-            description={this.state.description} 
-            error={this.state.error}
-          />
+          <div className="wrapper">
+              <div className="main">
+                  <div className="container">
+                      <div className="row">
+                        <div classname="col-xs-5 title-container">
+                            <Titles />                          
+                        </div>
+                        <div classname="col-xs-7 form-container">
+                            <Form getWeather={this.getWeather}/>   
+                            <Weather 
+                                temperature={this.state.temperature} 
+                                city={this.state.city} 
+                                country={this.state.country} 
+                                humidity={this.state.humidity} 
+                                description={this.state.description} 
+                                error={this.state.error}
+                            />
+                        </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
     </div>
         );
 }
