b0e1057c492d1b562ca0a576ce662d7a2f1ce443
tschedin <toms@tempworks.com>
2019-10-02 11:14:00 -0500

    Fix to the API call variables

--- src/WeatherAppTEST.js
+++ src/WeatherAppTEST.js
@@ -10,7 +10,9 @@ const API_KEY = "d798f5a297dd3b2e788deb6074ca3429";
 class WeatherApp extends React.Component {
     getWeather = async (e) => {
         e.preventDefault();
-        const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}`);
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
         const data = await apiCall.json();
         console.log(data);
     }
