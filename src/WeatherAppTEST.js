7ef6f07b5e86d007ee761fea126f16303333923f
tschedin <toms@tempworks.com>
2019-10-02 10:42:19 -0500

    saving api key

--- src/WeatherAppTEST.js
+++ src/WeatherAppTEST.js
@@ -2,6 +2,10 @@ import React from "react";
 
 import Titles from "./Components/titles";
 import Form from "./Components/form";
+import Weather from "./Components/weather";
+
+const API_KEY = "d798f5a297dd3b2e788deb6074ca3429";
+// http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={}
 
 class WeatherApp extends React.Component {
 render() {
@@ -9,6 +13,7 @@ render() {
     <div>
           <Titles />      
           <Form />      
+          <Weather />
     </div>
         );
 }
