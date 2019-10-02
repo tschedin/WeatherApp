272b4b3224179fb86ef349f92dbc5d86206b468e
tschedin <toms@tempworks.com>
2019-10-02 11:40:23 -0500

    Add labels to the return data and when to show

--- src/Components/weather.js
+++ src/Components/weather.js
@@ -4,12 +4,11 @@ class Weather extends React.Component {
     render() {
         return(
             <div>
-                {this.props.city}
-                {this.props.country}
-                {this.props.temperature}
-                {this.props.humidity}
-                {this.props.description}
-                {this.props.error}
+                { this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p> }
+                
+                { this.props.temperature && <p>Temperature: {this.props.temperature}</p> }
+                { this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+                { this.props.description && <p>Conditions: {this.props.description}  </p> }
             </div>            
         );
     }    
