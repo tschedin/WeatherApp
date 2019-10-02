39781dccf639276dde18f739f81a30f789e45a82
tschedin <toms@tempworks.com>
2019-10-02 18:49:50 -0500

    reinstalled bootstrap to fix DOM issue

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./weatherapp.css";
-import WeatherApp from "./WeatherApp";
-import registerServiceWorker from "./registerServiceWorker";
+import WeatherApp from './WeatherApp';
+import registerServiceWorker from './registerServiceWorker';
 
 var destination = document.querySelector("#container");
 
-ReactDOM.render(<WeatherApp />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<WeatherApp />, document.getElementById('root'));
+// registerServiceWorker();
+
+ReactDOM.render(
+    <div>
+    <WeatherApp />
+    </div>,
+    destination
+    );
