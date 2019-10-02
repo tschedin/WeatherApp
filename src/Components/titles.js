14c540595e5b49ef96967bc01be887499aea96b1
tschedin <toms@tempworks.com>
2019-10-02 16:03:55 -0500

    Changing form and titles to stateless also

--- src/Components/titles.js
+++ src/Components/titles.js
@@ -1,15 +1,10 @@
 import React from "react";
 
-class Titles extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Weather Finder</h1>
-                <p>Find out detailed information for weather!                   
-                </p>
-            </div>
-        );
-    }
-};
+const Titles = () => (
+<div>
+    <h1>Weather Finder</h1>
+    <p>Find out detailed information for weather!</p>
+</div>
+);
 
 export default Titles;
