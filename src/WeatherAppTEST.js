2531bae1381a130f086d3830e178127d2af4f89e
tschedin <toms@tempworks.com>
2019-10-02 11:22:47 -0500

    get and query state from API

--- src/WeatherAppTEST.js
+++ src/WeatherAppTEST.js
@@ -8,6 +8,14 @@ const API_KEY = "d798f5a297dd3b2e788deb6074ca3429";
 
 
 class WeatherApp extends React.Component {
+    state = {
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: undefined
+    }
     getWeather = async (e) => {
         e.preventDefault();
         const city = e.target.elements.city.value;
@@ -15,6 +23,13 @@ class WeatherApp extends React.Component {
         const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
         const data = await apiCall.json();
         console.log(data);
+        this.setState({
+            temperature: data.main.temp,
+            city: data.name,
+            country: data.sys.country,
+            humidity: data.main.humidity,
+            description: data.weather[0].description
+        })
     }
 render() {
     return (
