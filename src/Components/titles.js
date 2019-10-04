9a398750b1851a20003a7c08bf4191e232a7a29a
tschedin <toms@tempworks.com>
2019-10-04 18:12:41 -0500

    related to last update

--- src/Components/titles.js
+++ src/Components/titles.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Titles = () => (
 <div>
-    <h1>Weather Details</h1>
-    <p>Find out detailed information for the city you choose!</p>
+    <h1 className="title-container__title">Weather Details</h1>
+    <p className="title-container__subtitle">Find out detailed information for the city you choose!</p>
 </div>
 );
 
