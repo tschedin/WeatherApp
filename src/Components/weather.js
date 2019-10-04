50a8bbcc0ba6aba25d8eca3a21ea99b5f588535f
tschedin <toms@tempworks.com>
2019-10-04 18:12:24 -0500

    start changing styling for return info

--- src/Components/weather.js
+++ src/Components/weather.js
@@ -1,12 +1,32 @@
 import React from "react";
 
 const Weather = props => (
-    <div>
-    { props.city && props.country && <p>Location: {props.city}, {props.country}</p> }                
-    { props.temperature && <p>Temperature: {props.temperature}</p> }
-    { props.humidity && <p>Humidity: {props.humidity}</p>}
-    { props.description && <p>Conditions: {props.description}  </p> }
-    { props.error && <p>{props.error}</p> }
+    <div className="weather__info">
+    { 
+        props.city && props.country && <p>Location: 
+        {props.city}, {props.country}
+        </p> 
+        }                
+    { 
+        props.temperature && <p>Temperature: 
+        {props.temperature}
+        </p> 
+        }
+    { 
+        props.humidity && <p>Humidity: 
+        {props.humidity}
+        </p>
+        }
+    { 
+        props.description && <p>Conditions: 
+        {props.description} 
+        </p> 
+        }
+    { 
+        props.error && <p>
+        {props.error}
+        </p> 
+        }
     </div>  
 );
 
